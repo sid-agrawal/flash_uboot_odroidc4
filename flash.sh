d0c545862083b39189dac4cfb26eff62a67bf679
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2023-08-18 17:02:01 +1000

    Add capability to build U-Boot as well

--- flash.sh
+++ flash.sh
@@ -2,6 +2,20 @@
 
 set -e
 
+DEV=$1
+
+[ "$#" -ne 1 ] && echo "usage: ./flash.sh /path/to/device" && exit 1
+
+[ ! -f $1 ] && echo "ERROR: The device to write to '${1}' does not exist" && exit 1
+
+# First build U-Boot image
+CONFIG_PATH=$(pwd)/u-boot-config
+cd u-boot
+make clean
+cp $CONFIG_PATH .config
+make -j$(nproc)
+cd ..
+
 OUTPUT_DIR=output
 UBOOT_BIN_PATH=$(pwd)/u-boot/u-boot.bin
 
@@ -10,6 +24,6 @@ rm -rf $OUTPUT_DIR
 mkdir $OUTPUT_DIR
 ./build-fip.sh odroid-c4 $UBOOT_BIN_PATH $OUTPUT_DIR
 
-DEV=/dev/sda
 sudo dd if=$OUTPUT_DIR/u-boot.bin.sd.bin of=$DEV conv=fsync,notrunc bs=512 skip=1 seek=1
 sudo dd if=$OUTPUT_DIR/u-boot.bin.sd.bin of=$DEV conv=fsync,notrunc bs=1 count=440
+
